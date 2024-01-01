cfd1e899f90830ad88259954a44a12797a5d4ece
Trillionclues <trillionclues@gmail.com>
2024-01-01 20:58:26 +0100

    first commit

---
+++ src/components/ThemeRegistry/theme.ts
@@ -0,0 +1,30 @@
+import { Space_Grotesk } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const SpaceGrotesk = Space_Grotesk({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+  },
+  typography: {
+    fontFamily: SpaceGrotesk.style.fontFamily,
+  },
+  components: {
+    MuiAlert: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.severity === "info" && {
+            backgroundColor: "#60a5fa",
+          }),
+        }),
+      },
+    },
+  },
+});
+
+export default theme;
